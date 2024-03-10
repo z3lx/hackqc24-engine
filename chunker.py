bad17fb8b1b121dcc41bea36d37b99e8d0860a7f
Andy <andy.hou1604@gmail.com>
2024-03-09 20:53:42 -0500

    split text and create chunks

--- chunker.py
+++ chunker.py
@@ -42,10 +42,28 @@ def create_metadata(row:list[str], format:dict) -> dict:
     
     return metadata
 
-
+def chunk(dataset: list[list[str]], format:dict, index_of_description: str) -> list[list[str]]:
+    """
+    Chunks a dataset into by the different information and corresponding metadata.
+    
+    Args:
+        dataset(list[list[[str]]): The CSV file we want to chunk, must contain a list of rows, each row containing a list of information
+        format (dict): The way the metadata is stored in a particular CSV file, type_of_information:index (ie: {title:0, publication date:1, url:2})
+        index_of_description (str): Index of where the information is in the CSV file
+        
+    Returns:
+        list[list[str]]: List containing a chunk, each chunk has a text and its metadata.
+    """
+    
+    for row in first_dataset: 
+        metadata = create_metadata(row=row, format=format)
+        textChunks = TokenTextSplitter(chunk_size=4096, chunk_overlap = 20).split_text(text=row[index_of_description])
+        for text in textChunks:
+            chunks.append([text, metadata])
     
 
 if __name__ == "__main__":
     chunks = []
     
     first_dataset = parse_dataset("datasets\\Communiqués de presse\\Communiqués de presse (2023 à aujourd'hui).csv")
+    chunk(dataset=first_dataset, format={"title":0,"publication date":1,"url":2,"district service":4,"source":5}, index_of_description=3)
