bf103f80c7700e56ad0210a3d9373e02b680b5a2
Andy <andy.hou1604@gmail.com>
2024-03-11 20:38:22 -0400

    removed csv processing, moved to web-scrape branch

--- chunker.py
+++ chunker.py
@@ -3,48 +3,6 @@ from langchain_text_splitters import TokenTextSplitter
 from langchain.schema.document import Document
 import json
 
-def parse_dataset(dataset_path:str) -> list[list[str]]:
-    """
-    Parses a CSV dataset.
-    
-    Args:
-        dataset_path(str): The project path to the dataset.
-        
-    Returns:
-        list[list[str]]: The dataset as a list, each item is a row in the dataset, each row contains a list of information.
-    """
-    
-    dataset = []
-    with open(dataset_path, encoding="utf-8") as csvfile:
-        reader = csv.reader(csvfile)
-        next(reader) # Skip the first row, it only contains column titles
-        for row in reader:
-            dataset.append(row)
-    
-    return dataset
-
-def create_metadata(row:list[str], format:dict) -> dict:
-    """
-    Creates the metadata for a specific row in a CSV file.
-    
-    Args:
-        row (list[str]): The row we want to create metadata for.
-        format (dict): The way the metadata is stored in a particular CSV file, type_of_information:row_index (ie: {title:0, publication date:1, url:2})
-        
-    Returns:
-        dict: The metadata for the given row.
-    """
-    
-    metadata = {}
-    for type_of_information in format.keys():
-        try:
-            row_index = format[type_of_information]
-            metadata[type_of_information] = row[row_index]
-        except IndexError:
-            metadata[type_of_information] = None
-    
-    return metadata
-
 def chunkAsDocument(dataset: list[list[str]], format:dict, index_of_description: int) -> list[Document]:
     """
     Chunks a dataset by the different information and corresponding metadata into a list of Document objects.
