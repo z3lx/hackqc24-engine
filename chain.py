e1e10cc85d17e5a20bb874f8b558646314c981d5
z3lx <57017122+z3lx@users.noreply.github.com>
2024-03-12 17:00:15 -0400

    Add memory to chain

--- chain.py
+++ chain.py
@@ -1,13 +1,17 @@
 import os
+from operator import itemgetter
 
+from langchain.memory import ConversationBufferMemory
 from langchain_community.vectorstores.chroma import Chroma
+from langchain_core.messages import SystemMessage, HumanMessage
 from langchain_core.output_parsers import StrOutputParser
-from langchain_core.prompts import ChatPromptTemplate, PromptTemplate
-from langchain_core.runnables import RunnablePassthrough
+from langchain_core.prompts import ChatPromptTemplate, PromptTemplate, MessagesPlaceholder
+from langchain_core.runnables import RunnablePassthrough, RunnableLambda
 from langchain_openai import OpenAIEmbeddings, ChatOpenAI
 
 os.environ["LANGCHAIN_TRACING_V2"] = "true"
 
+# Language chain
 lang_prompt = PromptTemplate.from_template(
     "What language is the following text written in?\n"
     "text=\"{text}\"\n"
@@ -24,9 +28,9 @@ lang_chain = (
     | StrOutputParser()
 )
 
-embeddings = OpenAIEmbeddings(model="text-embedding-3-small")
 model = ChatOpenAI(model="gpt-3.5-turbo-0125")
-
+embeddings = OpenAIEmbeddings(model="text-embedding-3-small")
+memory = ConversationBufferMemory(return_messages=True)
 db = Chroma(
     persist_directory="./chromadb",
     embedding_function=embeddings
@@ -34,25 +38,29 @@ db = Chroma(
 retriever = db.as_retriever(search_kwargs={"k": 1})
 # docs = db.similarity_search(question)
 
-template = """Answer the question with the following context:
-{context}
-
-Question: {question}
-Answer the question in {language}.
-"""
-prompt = ChatPromptTemplate.from_template(template)
+prompt = ChatPromptTemplate.from_messages([
+    MessagesPlaceholder(variable_name="history"),
+    ("system", "Answer the user with the following context:\n{context}"),
+    ("user", "{message}")
+])
 
 chain = (
     {"context": retriever,
-     "question": RunnablePassthrough(),
-     "language": lang_chain
+     "message": RunnablePassthrough(),
+     #"language": lang_chain
      }
+    | RunnablePassthrough.assign(
+        history=RunnableLambda(memory.load_memory_variables) | itemgetter("history")
+    )
     | prompt
     | model
-    | StrOutputParser()
 )
 
 while True:
-    question = input("")
-    answer = chain.invoke(question)
-    print(answer)
+    message = input("")
+    response = chain.invoke(message)
+    memory.save_context(
+        inputs={"input": message},
+        outputs={"output": response.content}
+    )
+    print(response.content)
