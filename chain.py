0f23b6444e094dea13018452fe2ebeab7f586192
z3lx <57017122+z3lx@users.noreply.github.com>
2024-03-12 14:05:45 -0400

    Add language specification to chain

--- chain.py
+++ chain.py
@@ -2,13 +2,29 @@ import os
 
 from langchain_community.vectorstores.chroma import Chroma
 from langchain_core.output_parsers import StrOutputParser
-from langchain_core.prompts import ChatPromptTemplate
+from langchain_core.prompts import ChatPromptTemplate, PromptTemplate
 from langchain_core.runnables import RunnablePassthrough
 from langchain_openai import OpenAIEmbeddings, ChatOpenAI
 
 os.environ["LANGCHAIN_TRACING_V2"] = "true"
 
-embeddings = OpenAIEmbeddings(model="text-embedding-3-large")
+lang_prompt = PromptTemplate.from_template(
+    "What language is the following text written in?\n"
+    "text=\"{text}\"\n"
+    "Reply in a singular word."
+)
+lang_model = ChatOpenAI(
+    model="gpt-3.5-turbo-0125",
+    temperature=0
+)
+lang_chain = (
+    {"text": RunnablePassthrough()}
+    | lang_prompt
+    | lang_model
+    | StrOutputParser()
+)
+
+embeddings = OpenAIEmbeddings(model="text-embedding-3-small")
 model = ChatOpenAI(model="gpt-3.5-turbo-0125")
 
 db = Chroma(
@@ -22,11 +38,15 @@ template = """Answer the question with the following context:
 {context}
 
 Question: {question}
+Answer the question in {language}.
 """
 prompt = ChatPromptTemplate.from_template(template)
 
 chain = (
-    {"context": retriever, "question": RunnablePassthrough()}
+    {"context": retriever,
+     "question": RunnablePassthrough(),
+     "language": lang_chain
+     }
     | prompt
     | model
     | StrOutputParser()
