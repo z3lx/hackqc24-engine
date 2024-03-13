b137c93ccdb572357c4fcaf4f1b638c0956b4393
ParsaJafarian <parsajafarian72@gmail.com>
2024-03-12 22:27:00 -0400

    Add data loading functios

--- data_utils.py
+++ data_utils.py
@@ -5,7 +5,7 @@ import json
 Utility functions for saving data to different formats
 """
     
-def save_to_json(data: dict, filename: str , dir: str = ""):
+def save_json(data: dict, filename: str , dir: str = ""):
     """
     Save data to a json file with utf-8 encoding.
 
@@ -19,7 +19,35 @@ def save_to_json(data: dict, filename: str , dir: str = ""):
     print(f"Saved to {dir}{filename}.json")
 
 #@TODO: Add support for csv. The function below is broken
-def save_to_csv(self, data: dict, filename: str, dir: str = ""):
+def save_csv(self, data: dict, filename: str, dir: str = ""):
     df = pd.DataFrame(data)
     df.to_csv(f"{dir}{filename}.csv", index=False, encoding="utf-8")
     print(f"Saved to {dir}{filename}.csv")
+    
+def load_json(filename: str, dir: str = "") -> dict:
+    """
+    Load a json file
+
+    Args:
+        filename (str): name of the file
+        dir (str, optional): directory of the file. Defaults to "".
+
+    Returns:
+        dict: loaded data
+    """
+    with open(f"{dir}{filename}.json", "r", encoding="utf-8") as file:
+        data = json.load(file)
+    return data
+
+def load_csv(filename: str, dir: str = "") -> pd.DataFrame:
+    """
+    Load a csv file
+
+    Args:
+        filename (str): name of the file
+        dir (str, optional): directory of the file. Defaults to "".
+
+    Returns:
+        pd.DataFrame: loaded data
+    """
+    return pd.read_csv(f"{dir}{filename}.csv", encoding="utf-8")
