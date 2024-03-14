f31d4c252e35019ff4133e6981a94ccae569d333
z3lx <57017122+z3lx@users.noreply.github.com>
2024-03-14 13:10:11 -0400

    Add argument parser to metadata filter

--- scripts/filter.py
+++ scripts/filter.py
@@ -1,3 +1,4 @@
+import argparse
 from typing import List
 
 from langchain_core.documents import Document
@@ -17,3 +18,24 @@ def filter_metadata(docs: List[Document], keys: List[str]) -> List[Document]:
     for doc in docs:
         doc.metadata = {k: v for k, v in doc.metadata.items() if k in keys}
     return docs
+
+
+if __name__ == "__main__":
+    import os
+    import sys
+    sys.path.append(os.path.dirname(os.path.dirname(os.path.abspath(__file__))))
+    from utils.document import get_documents, save_documents
+
+    parser = argparse.ArgumentParser(description="Filter documents metadata.")
+    parser.add_argument("--input", type=str, required=True,
+                        help="The input directory containing the documents.")
+    parser.add_argument("--output", type=str, required=True,
+                        help="The output directory to save the filtered documents.")
+    parser.add_argument("--keys", type=str, nargs='+', required=True,
+                        help="The keys to keep in the metadata.")
+
+    args = parser.parse_args()
+
+    docs = get_documents(path=args.input)
+    docs = filter_metadata(docs=docs, keys=args.keys)
+    save_documents(path=args.output, documents=docs)
