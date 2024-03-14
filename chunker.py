b342b7ecd0b7c830328ec8da997d04d7eb107c67
z3lx <57017122+z3lx@users.noreply.github.com>
2024-03-14 01:27:26 -0400

    Refactor chunking function

--- chunker.py
+++ chunker.py
@@ -1,42 +1,32 @@
-import json
-import os
-
 from langchain.docstore.document import Document
 from langchain_text_splitters import TokenTextSplitter
 
 
-def chunk(path: str, documents: list[Document], save_as_file:bool, save_as_list:bool) -> list[Document]:
+def chunk(documents: list[Document],
+          chunk_size: int = 4000, chunk_overlap: int = 200,
+          model_name: str = "gpt-3.5-turbo-0125") -> list[Document]:
     """
-    Chunks a list of unchunked Documents into chunked Documents
+    Splits a list of documents into chunks.
+
+    This function takes a list of documents and splits each document into chunks of a specified size.
+    The chunks can overlap, and the size of the overlap can also be specified.
+    The function uses a specific model for splitting the documents.
+    If a path is provided, the function will save the chunked documents to that path.
 
     Args:
-        documents (list[Document]): List of unchunked Documents we want to chunk.
-        save_as_file (boolean): Says if we want to write all the chunks as txt and json files, writes in "chunks" project directory.
-        save_as_list (boolean): Says if we want to save all the chunks as a list of Documents in memory.
+        documents (list[Document]): The list of documents to be chunked.
+        path (str, optional): The path where the chunked documents will be saved. Defaults to None.
+        chunk_size (int, optional): The size of each chunk. Defaults to 4000.
+        chunk_overlap (int, optional): The size of the overlap between chunks. Defaults to 200.
+        model_name (str, optional): The name of the model to be used for splitting the documents.
+            Defaults to "gpt-3.5-turbo-0125".
 
     Returns:
-        list[Document]: A list of Documents if save_as_list is true, returns Nothing otherwise.
+        list[Document]: The list of chunked documents.
     """
-
-    # Remove everything in "chunks" project folder
-    for file in os.listdir("chunks"):
-        os.remove(f"chunks/{file}")
-
-    text_splitter = TokenTextSplitter()
-    text_splitter._chunk_size = 4096
-    text_splitter._chunk_overlap = 20
-    document_split = text_splitter.split_documents(documents)
-
-    if save_as_file:
-        for index, document in enumerate(document_split):
-            with open(f"chunks/chunk{index}.txt", "w", encoding="utf-8") as f:
-                f.write(document.page_content)
-            with open(f"chunks/chunk{index}.meta", "w", encoding="utf-8") as f:
-                json.dump(document.metadata, f, ensure_ascii=False)
-                # Print progress
-            print(f"Document {index+1}/{len(document_split)} created       {round(((index+1)/len(document_split) * 100), 2)}% Done")
-
-    if save_as_list:
-        return document_split
-
-    return None
+    splitter = TokenTextSplitter.from_tiktoken_encoder(
+        chunk_size=chunk_size,
+        chunk_overlap=chunk_overlap,
+        model_name=model_name,
+    )
+    return splitter.split_documents(documents)
